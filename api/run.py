becdf2d4b1446daca2a84797f0a7417d65fc53b3
Zeb <zeb@tekydevelop.com>
2020-04-15 21:28:16 +0100

    Code refactoring

--- api/run.py
+++ api/run.py
@@ -7,7 +7,7 @@ def create_app(config_filename):
      from app import api_bp
      app.register_blueprint(api_bp, url_prefix='/api')
 
-     from Model import db
+     from models.device import db
      db.init_app(app)
 
      return app
